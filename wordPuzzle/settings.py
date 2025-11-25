537ba4e3b13ff6414f9e579630e5f52798321117
mohamedamaadour <mohamedamaadou@gmail.com>
2025-11-25 12:38:37 +0100

    the sixth commit

--- wordPuzzle/settings.py
+++ wordPuzzle/settings.py
@@ -31,8 +31,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY'),
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False') == 'True',
 
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -141,3 +139,5 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
